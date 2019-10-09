2ab45700a9dd533e55c0b88bcd796e4e8cc8184f
Bernardo Lourenço <bernardo.lourenco@ua.pt>
2019-10-09 17:07:24 +0100

    added the laserscan detectors

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup
+import setuptools
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import sys
+import os
 
 setup(
     name="multimodal_calibration",
@@ -6,6 +10,9 @@ setup(
     description="A package for the calibration of multiple sensors",
     author="bernardomig",
     author_email="bernardo.lourenco@ua.pt",
-    packages=['multimodal_calibration'],
-    install_requires=['numpy >= 1.16.5', 'opencv-python >= 4.1.0.25'],
+    packages=
+        find_packages(),
+    install_requires=[
+        'numpy >= 1.16.5', 
+        'opencv-python >= 4.1.0.25'],
 )
